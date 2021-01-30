c8fbdfbaaf773306178518fbba473b4d4f594171
canuck <darrenw@byui.edu>
2021-01-30 15:46:08 -0700

    Darren modifications

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -1,9 +1,20 @@
 package prove04;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, my name is Dana.");
+        String yourName = "Dana";
+        List<String> teamMatesNames = new ArrayList<>();
+        teamMatesNames.add("Darren");
+
+        System.out.printf("Hello, my name is %s", yourName + "!\n");
+
+        for (String name : teamMatesNames             ) {
+            System.out.printf("Hello, " + yourName + " my name is %s!%n", name);
+        }
+
     }
 }
